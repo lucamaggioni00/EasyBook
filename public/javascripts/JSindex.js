9cf9c61d9506e17b96adfd0fd20b4c49c91a5fee
lucamaggioni00 <brostus2k@gmail.com>
2022-05-20 00:11:37 +0200

    modifiche secondarie

--- public/javascripts/JSindex.js
+++ public/javascripts/JSindex.js
@@ -6,3 +6,6 @@ function $(id) {
 	return document.getElementById(id);
 } 
 
+/*  TODO: GUARDARE PER PRENDERE IL VALORE SELEZIONATO DEL TAVOLO
+https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_option_value
+*/
