1a6f3a871d3d17b695265a95b92298165ae41aeb
lucamaggioni00 <brostus2k@gmail.com>
2022-05-20 00:36:23 +0200

    aggiunta funzione per ntavolo

--- public/javascripts/JSindex.js
+++ public/javascripts/JSindex.js
@@ -1,11 +1,14 @@
 /* --- Scripts della pagina index.html --- */
 
-
 /* parametrizzazione della funzione per getElementById() */
 function $(id) { 
 	return document.getElementById(id);
 } 
 
-/*  TODO: GUARDARE PER PRENDERE IL VALORE SELEZIONATO DEL TAVOLO
-https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_option_value
-*/
+
+/* acquisisce il numero del tavolo */
+function selectTavolo(){
+	var x = $('mySelect').selectedIndex; // x = indice del valore selezionato
+	return document.getElementsByTagName('option')[x].value; 
+	
+}
