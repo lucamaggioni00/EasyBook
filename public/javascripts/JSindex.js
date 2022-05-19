bd308f6a3e6d85e0f3c93f8a0a806d7266fa939e
lucamaggioni00 <brostus2k@gmail.com>
2022-05-20 00:37:14 +0200

    Update JSindex.js

--- public/javascripts/JSindex.js
+++ public/javascripts/JSindex.js
@@ -6,7 +6,7 @@ function $(id) {
 } 
 
 
-/* acquisisce il numero del tavolo */
+/* acquisisce il numero del tavolo TODO METTERLA DA QUALCHE PARTE*/
 function selectTavolo(){
 	var x = $('mySelect').selectedIndex; // x = indice del valore selezionato
 	return document.getElementsByTagName('option')[x].value; 
