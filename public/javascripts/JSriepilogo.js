cd2695c0e24de95ab751a46c2b1529103caed246
lucamaggioni00 <brostus2k@gmail.com>
2022-07-09 16:20:26 +0200

    inizio costruzione riepilogo

---
+++ public/javascripts/JSriepilogo.js
@@ -0,0 +1,22 @@
+/* --- Scripts della pagina riepilogo.html --- */
+
+scontrino();
+
+/* costruisce tutto il riepilogo dallo storage */
+function scontrino() {
+	
+	// inserisce numTavolo
+	$('nTavoloRecap').innerText = "Tavolo: " + (sessionStorage.getItem("nTavolo"));
+
+	// inserisce i valori nella griglia
+	for (let i=0; i < 20; i++) { 
+		//console.log("Dal session storage si ha: i=" + i + " con valore=" + sessionStorage.getItem(i));
+		$("contatore"+i).innerText = sessionStorage.getItem(i);
+		//TODO inserire anche nome e prezzo
+	}
+}
+
+/* parametrizzazione della funzione per getElementById() */
+function $(id) { 
+	return document.getElementById(id);
+}
