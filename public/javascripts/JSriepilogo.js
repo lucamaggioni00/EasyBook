0573c4ede72b8d194e1e766b06a7b4091b03e9e9
lucamaggioni00 <brostus2k@gmail.com>
2022-07-10 01:14:29 +0200

    add ajax call per riepilogo

--- public/javascripts/JSriepilogo.js
+++ public/javascripts/JSriepilogo.js
@@ -1,6 +1,11 @@
 /* --- Scripts della pagina riepilogo.html --- */
 
-scontrino();
+
+
+
+
+
+scontrino(); // chiamata per la costruzione della tabella di riepilogo
 
 /* costruisce tutto il riepilogo dallo storage */
 function scontrino() {
@@ -12,14 +17,33 @@ function scontrino() {
 	else {
 		$('nTavoloRecap').innerText = "Tavolo: " + (sessionStorage.getItem("nTavolo"));
 	}
-	
 
-	// inserisce i valori nella griglia
-	for (let i=0; i < 20; i++) { 
-		//console.log("Dal session storage si ha: i=" + i + " con valore=" + sessionStorage.getItem(i));
-		$("contatore"+i).innerText = sessionStorage.getItem(i);
-		//TODO inserire anche nome e prezzo
-	}
+	/* chiamata AJAX per menu */
+	const xhttp = new XMLHttpRequest();
+
+	xhttp.onreadystatechange = function() { // definisco cosa fa quando l'oggetto cambia stato. tipo quando la risposta è pronta
+		if (this.readyState == 4 && this.status == 200) { // tutto ok
+    		var menu = JSON.parse(xhttp.responseText); // parsa la stringa json in un oggetto javascript
+
+			// inserisce i valori nella griglia
+			for (let i=0; i < 20; i++) { 
+				//console.log("Dal session storage si ha: i=" + i + " con valore=" + sessionStorage.getItem(i));
+				$("contatore"+i).innerText = sessionStorage.getItem(i);
+				
+				//TODO inserire anche nome e prezzo
+				if(sessionStorage.getItem(i) > 0) { // se ha ordinato almeno una porzione
+					$("piatto"+i).innerText = menu[i].Nome;
+					$("prezzo"+i).innerText = "€ " + menu[i].Prezzo;
+				}
+			}
+    	}
+	};
+	xhttp.open("GET", "../others/menu1.json", true);
+	//console.log("Richiesta creata");
+	xhttp.send();
+	//console.log("Richiesta mandata");
+
+
 }
 
 /* parametrizzazione della funzione per getElementById() */
